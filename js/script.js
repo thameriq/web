b693a156662c889bd18f33df0b80dc43909334d3
thameriq <tmmiva@gmail.com>
2025-01-11 21:59:37 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing", {
-  strings: ["Programmer", "Cybersecurity", "Scripter"],
+  strings: ["Programmer", "Web Designer", "Scripter"],
   typeSpeed: 100,
   BackSpeed: 60,
   loop: true,
